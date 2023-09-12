3f87e44520942a2dd19c7040fbb4174f3ffe0a09
Draco3310 <131842369+Draco3310@users.noreply.github.com>
2023-09-11 19:59:19 -0600

    Update Updated_Risk_Management.py

--- Updated_Risk_Management.py
+++ Updated_Risk_Management.py
@@ -9,8 +9,10 @@ def advanced_risk_management(portfolio_value, alpha=0.05):
     
     # Calculate CVaR
     CVaR = np.mean([x for x in historical_data if x <= VaR])
+
+    # Sharpe Ratio
+    sharpe_ratio = np.mean(historical_data) / np.std(historical_data)
     
-    # Dynamic position sizing based on VaR and CVaR
-    position_size = portfolio_value * (1 - VaR)
-    
+   # Dynamic position sizing
+    position_size = portfolio_value * (1 - VaR) * sharpe_ratio
     return position_size
