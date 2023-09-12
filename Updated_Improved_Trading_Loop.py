099fbdce684b3c8aab692023b17579344f4cdadc
Draco3310 <131842369+Draco3310@users.noreply.github.com>
2023-09-11 19:32:22 -0600

    Update Updated_Improved_Trading_Loop.py

--- Updated_Improved_Trading_Loop.py
+++ Updated_Improved_Trading_Loop.py
@@ -1,3 +1,5 @@
+import talib
+
 buy_price = None
 
 while True:
@@ -6,19 +8,18 @@ while True:
     ohlcv = safe_execute(kraken.fetch_ohlcv, symbol, timeframe)
     df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
 
-    if not validate_data(df):
+    if not enhanced_validate_data(df):
         continue
     
-    k, d = talib.STOCHRSI(df['close'])
-    buy_signal, sell_signal = generate_signal(k, d, df['volume'])
+    final_signal = ensemble_trading_strategy(df)
     
     current_price = df['close'][-1]
     
-    if buy_signal and buy_price is None:
+    if final_signal == 'buy' and buy_price is None:
         safe_execute(place_order, 'buy', symbol, amount, current_price)
         buy_price = current_price
         logging.info(f"Buy order executed at {current_price}")
-    elif sell_signal or check_stop_loss_or_take_profit(current_price, buy_price) in ['stop_loss', 'take_profit']:
+    elif final_signal == 'sell' or advanced_risk_management(current_price, buy_price) == 'sell':
         safe_execute(place_order, 'sell', symbol, amount, current_price)
         buy_price = None
         logging.info(f"Sell order executed at {current_price}")
