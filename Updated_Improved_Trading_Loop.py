cb91a49d88916826f08b7207a78d9b38755018b8
Draco3310 <131842369+Draco3310@users.noreply.github.com>
2023-09-11 19:56:34 -0600

    Update Updated_Improved_Trading_Loop.py

--- Updated_Improved_Trading_Loop.py
+++ Updated_Improved_Trading_Loop.py
@@ -1,20 +1,19 @@
 import talib
+from talib import MA_Type
 
 buy_price = None
 
 while True:
     time.sleep(1)
-    
     ohlcv = safe_execute(kraken.fetch_ohlcv, symbol, timeframe)
     df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
 
     if not enhanced_validate_data(df):
         continue
-    
+
     final_signal = ensemble_trading_strategy(df)
-    
     current_price = df['close'][-1]
-    
+
     if final_signal == 'buy' and buy_price is None:
         safe_execute(place_order, 'buy', symbol, amount, current_price)
         buy_price = current_price
