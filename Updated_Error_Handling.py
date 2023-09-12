60360fad4aaf03ca3ce98984da094ea7cffa0415
Draco3310 <131842369+Draco3310@users.noreply.github.com>
2023-09-11 19:54:56 -0600

    Update Updated_Error_Handling.py

--- Updated_Error_Handling.py
+++ Updated_Error_Handling.py
@@ -1,11 +1,21 @@
 from pybreaker import CircuitBreaker
 
+# Custom exception classes
+class APIError(Exception):
+    pass
+
+class DataValidationError(Exception):
+    pass
+
 breaker = CircuitBreaker(fail_max=3, reset_timeout=60)
 
 @breaker
 def safe_execute(func, *args, **kwargs):
     try:
         return func(*args, **kwargs)
+    except APIError as e:
+        log_error("APIError", str(e))
+    except DataValidationError as e:
+        log_error("DataValidationError", str(e))
     except Exception as e:
-        # Switch to a backup API or data source
-        return backup_func(*args, **kwargs)
+        log_error("UnknownError", str(e))
