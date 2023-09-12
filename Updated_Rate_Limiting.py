1594037485590810a63504e47d4bbd112ba57ea4
Draco3310 <131842369+Draco3310@users.noreply.github.com>
2023-09-11 19:33:02 -0600

    Update Updated_Rate_Limiting.py

--- Updated_Rate_Limiting.py
+++ Updated_Rate_Limiting.py
@@ -1,29 +1,12 @@
-
 import time
-from collections import deque
-from Error_Handling import safe_execute
 
-# Initialize rate-limiting variables
-request_queue = deque()
-rate_limit = 1.0  # Initial rate limit in seconds
-
-def rate_limited_request(func, *args, **kwargs):
-    global rate_limit
-    current_time = time.time()
-
-    # Dynamic Rate Limiting
-    while request_queue and current_time - request_queue[0] > 60:
-        request_queue.popleft()
-
-    if len(request_queue) >= rate_limit * 60:
-        time_to_wait = 60 + request_queue[0] - current_time
-        time.sleep(time_to_wait)
-
-    # Execute the API call safely
-    response = safe_execute(func, *args, **kwargs)
-
-    # Update rate limit based on API response (placeholder, to be filled in later)
-    # rate_limit = ...
-
-    request_queue.append(time.time())
+def adaptive_rate_limited_request(func, *args, **kwargs):
+    rate_limit = 1.0  # initial rate limit in seconds
+    while True:
+        response = func(*args, **kwargs)
+        if 'X-RateLimit-Remaining' in response.headers:
+            rate_limit = float(response.headers['X-RateLimit-Remaining'])
+        if rate_limit > 0:
+            break
+        time.sleep(rate_limit)
     return response
