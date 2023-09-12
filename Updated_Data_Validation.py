bbc83e67083d2c9c43019347d154ea499fb40d6c
Draco3310 <131842369+Draco3310@users.noreply.github.com>
2023-09-11 19:31:16 -0600

    Update Updated_Data_Validation.py

--- Updated_Data_Validation.py
+++ Updated_Data_Validation.py
@@ -1,22 +1,13 @@
-
+import numpy as np
 import pandas as pd
 
-def validate_data(df):
-    if df.empty or df.isnull().values.any():
-        return False, "Dataframe is empty or contains null values"
+def enhanced_validate_data(df):
+    # Outlier detection using Z-score
+    z_scores = np.abs(df[['open', 'high', 'low', 'close']].apply(zscore))
+    df = df[(z_scores < 3).all(axis=1)]
 
-    # Type Validation
-    expected_types = {'timestamp': 'int64', 'open': 'float64', 'high': 'float64', 'low': 'float64', 'close': 'float64'}
-    for column, expected_type in expected_types.items():
-        if df[column].dtype != expected_type:
-            return False, f"Unexpected data type in column {column}. Expected {expected_type}, got {df[column].dtype}"
-
-    # Value Range
-    if (df['open'] < 0).any() or (df['high'] < 0).any() or (df['low'] < 0).any() or (df['close'] < 0).any():
-        return False, "Negative values found in price columns"
-
-    # Temporal Consistency
-    if not df['timestamp'].is_monotonic_increasing:
-        return False, "Timestamps are not in increasing order"
+    # Data integrity check
+    if not (df['high'] >= df['low']).all():
+        return False, "Data integrity check failed"
 
     return True, "Data is valid"
