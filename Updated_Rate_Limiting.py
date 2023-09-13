7c47a40a8c6e648ac24bae57542a219f173ced48
Draco3310 <131842369+Draco3310@users.noreply.github.com>
2023-09-13 15:40:21 -0600

    Update Updated_Rate_Limiting.py

--- Updated_Rate_Limiting.py
+++ Updated_Rate_Limiting.py
@@ -1,12 +1,7 @@
 import time
+from ratelimit import limits, sleep_and_retry
 
-def adaptive_rate_limited_request(func, *args, **kwargs):
-    rate_limit = 1.0  # initial rate limit in seconds
-    while True:
-        response = func(*args, **kwargs)
-        if 'X-RateLimit-Remaining' in response.headers:
-            rate_limit = float(response.headers['X-RateLimit-Remaining'])
-        if rate_limit > 0:
-            break
-        time.sleep(rate_limit)
-    return response
+@sleep_and_retry
+@limits(calls=150, period=600)
+def rate_limited_api_call(api_function, *args, **kwargs):
+    return api_function(*args, **kwargs)
