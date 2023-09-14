48df28e3f131e05d4abf14706ed41a107967d623
Draco3310 <131842369+Draco3310@users.noreply.github.com>
2023-09-13 19:48:29 -0600

    Update Updated_Error_Handling.py

--- Updated_Error_Handling.py
+++ Updated_Error_Handling.py
@@ -2,8 +2,17 @@ import time
 from pybreaker import CircuitBreaker
 import sentry_sdk
 
-sentry_sdk.init("your_dsn_here")
-
+sentry_sdk.init(
+    dsn="https://5f11fa4041efbc2a2d40248970836357@o4505875776667648.ingest.sentry.io/4505875779158016",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 # Custom exception classes
 class APIError(Exception):
     pass
