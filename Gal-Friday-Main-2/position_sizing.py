40f2b957d4251bbe155548c056aae6decb0130af
Draco3310 <131842369+Draco3310@users.noreply.github.com>
2023-09-16 17:20:30 -0600

    Delete Gal-Friday-Main-2 directory

--- Gal-Friday-Main-2/position_sizing.py
+++
@@ -1,8 +0,0 @@
-# position_sizing.py
-
-def calculate_position_size(symbol, current_price, buy_price, portfolio_value):
-    """
-    Calculate the position size for a trade.
-    """
-    # Your existing logic here
-    return position_size
